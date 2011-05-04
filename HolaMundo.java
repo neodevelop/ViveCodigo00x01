f336a200b53d9efee044efb1d8a52d5bf14deeac
neodevelop <neodevelop@gmail.com>
2011-05-04 17:58:57 -0500

    Clase hola mundo en Java Creada...

---
+++ HolaMundo.java
@@ -0,0 +1,11 @@
+public class HolaMundo{
+  private int numero;
+  
+  public void setNumero(int numero){
+    this.numero = numero;
+  }
+  
+  public int getNumero(){
+    return this.numero;
+  }
+}
